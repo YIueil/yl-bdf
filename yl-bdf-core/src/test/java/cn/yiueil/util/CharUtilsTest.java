0eb665ddab075633db518e68aea020fe05a0d1cd
liuhao <8U%h&Xir#LyFI#>
2022-04-20 19:41:49 +0800

    字符工具类完善

---
+++ yl-bdf-core/src/test/java/cn/yiueil/util/CharUtilsTest.java
@@ -0,0 +1,13 @@
+package cn.yiueil.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharUtilsTest {
+
+    @Test
+    void isBlankChar() {
+        System.out.println(CharUtils.isBlankChar(' '));
+    }
+}
