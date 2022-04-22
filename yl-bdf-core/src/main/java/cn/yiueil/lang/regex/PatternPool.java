0a9c2acc36f798d266616239ef703db74d23c389
liuhao <8U%h&Xir#LyFI#>
2022-04-22 17:29:27 +0800

    验证工具类提供

---
+++ yl-bdf-core/src/main/java/cn/yiueil/lang/regex/PatternPool.java
@@ -0,0 +1,14 @@
+package cn.yiueil.lang.regex;
+
+import java.util.regex.Pattern;
+
+/**
+ * Author:YIueil
+ * Date:2022/4/22 17:12
+ * Description: 正则资源池
+ */
+public class PatternPool {
+    public static final Pattern PHONE = Pattern.compile(RegexPool.PHONE);
+    public static final Pattern MAIL = Pattern.compile(RegexPool.MAIL);
+    public static final Pattern ID_CARD = Pattern.compile(RegexPool.ID_CARD);
+}
---
+++ yl-bdf-core/src/main/java/cn/yiueil/util/CheckUtils.java
@@ -0,0 +1,31 @@
+package cn.yiueil.util;
+
+import cn.yiueil.lang.regex.PatternPool;
+
+import java.util.regex.Pattern;
+
+/**
+ * Author:YIueil
+ * Date:2022/4/22 17:14
+ * Description: 验证工具类
+ */
+public class CheckUtils {
+    public static boolean isPhone(String str) {
+        return validator(PatternPool.PHONE, str);
+    }
+
+    public static boolean isMail(String str) {
+        return validator(PatternPool.MAIL, str);
+    }
+
+    public static boolean isIDCard(String str) {
+        return validator(PatternPool.ID_CARD, str);
+    }
+
+    private static boolean validator(Pattern pattern, String str) {
+        if (StringUtils.isBlank(str)) {
+            return false;
+        }
+        return pattern.matcher(str).matches();
+    }
+}
