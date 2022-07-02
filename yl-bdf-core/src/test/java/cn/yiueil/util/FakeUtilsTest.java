c1f4f9432132500298650fa390c566b2a3067234
YIueil <YIueil@163.com>
2022-07-02 21:27:53 +0800

    随机类的完善

---
+++ yl-bdf-core/src/test/java/cn/yiueil/util/FakeUtilsTest.java
@@ -0,0 +1,29 @@
+package cn.yiueil.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FakeUtilsTest {
+
+    @Test
+    void getNum() {
+    }
+
+    @Test
+    void getPhone() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getIdCard() {
+    }
+
+    @Test
+    void getChineseName() {
+        System.out.println(FakeUtils.getChineseName());
+    }
+}
