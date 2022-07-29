155f59e9b08dc084dce75d50b17f3d6966f6eea7
YIueil <YIueil@163.com>
2022-07-29 16:36:46 +0800

    hibernate增强, 按照实体字段顺序创建表字段

--- yl-blog/src/main/java/cn/yiueil/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package cn.yiueil.repository;
-
-import cn.yiueil.entity.Blog;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BlogRepository extends CrudRepository<Blog, Integer> {
-}
