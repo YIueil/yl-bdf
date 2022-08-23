51936d65fbfcd146434890a933347c598d377eae
YIueil <YIueil@163.com>
2022-08-23 22:34:53 +0800

    Page相关增删改查增加

--- yl-blog/src/main/java/cn/yiueil/controller/PageController.java
+++ yl-blog/src/main/java/cn/yiueil/controller/PageController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/page")
 public class PageController implements LoggedController{
