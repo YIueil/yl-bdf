571bd7757b150495885943e7bb9299fb19a1409e
YIueil <YIueil@163.com>
2022-11-15 22:32:12 +0800

    增加用户账户模块

--- yl-bdf-orup/src/main/java/cn/yiueil/controller/ORUPController.java
+++ yl-bdf-orup/src/main/java/cn/yiueil/controller/ORUPController.java
@@ -3,6 +3,7 @@ package cn.yiueil.controller;
 import cn.yiueil.general.RestURL;
 import cn.yiueil.constant.ORUPRestURL;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,7 +13,8 @@ import javax.servlet.http.HttpServletRequest;
  * Date:2022/11/15 22:23
  * Description: 用户账户控制器
  */
-@RestController(value = RestURL.BASE_PATH + ORUPRestURL.ORUP)
+@RestController
+@RequestMapping(value = RestURL.BASE_PATH + ORUPRestURL.ORUP)
 public class ORUPController implements LoggedController{
     @GetMapping(value = "currentUser")
     public String currentUser(HttpServletRequest request) {
