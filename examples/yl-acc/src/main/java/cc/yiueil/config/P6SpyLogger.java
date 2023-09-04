33fef3a9e3809f1dd3aaa2f817a7ab5284608c7d
YIueil <YIueil@163.com>
2023-09-04 16:09:48 +0800

    docs: 文档编码完善

--- examples/yl-acc/src/main/java/cc/yiueil/config/P6SpyLogger.java
+++ examples/yl-acc/src/main/java/cc/yiueil/config/P6SpyLogger.java
@@ -7,19 +7,26 @@ import org.hibernate.engine.jdbc.internal.Formatter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * P6SpyLogger
+ *
+ * @author 弋孓 YIueil@163.com
+ * @version 1.0
+ * @date 2023/9/4 16:06
+ */
 public class P6SpyLogger implements MessageFormattingStrategy {
 
     private final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
 
-    private static final Formatter formatter;
+    private static final Formatter FORMATTER;
 
     static {
-        formatter = new BasicFormatterImpl();
+        FORMATTER = new BasicFormatterImpl();
     }
 
     @Override
     public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
         StringBuilder sb = new StringBuilder();
-        return !"".equals(sql.trim()) ? sb.append(this.format.format(new Date())).append(" | took ").append(elapsed).append("ms | ").append(category).append(" | connection ").append(connectionId).append(formatter.format(sql)).append(";").toString()  : "";
+        return !"".equals(sql.trim()) ? sb.append(this.format.format(new Date())).append(" | took ").append(elapsed).append("ms | ").append(category).append(" | connection ").append(connectionId).append(FORMATTER.format(sql)).append(";").toString() : "";
     }
 }
