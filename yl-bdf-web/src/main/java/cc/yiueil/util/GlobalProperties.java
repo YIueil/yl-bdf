414012bb89dad295bd2b51aa9179179284cb3e3a
YIueil <YIueil2163.com>
2024-03-19 17:58:19 +0800

    feat(yl-bdf-web): GlobalProperties全局配置工具类实现

--- yl-bdf-web/src/main/java/cc/yiueil/util/GlobalProperties.java
+++ yl-bdf-web/src/main/java/cc/yiueil/util/GlobalProperties.java
@@ -1,11 +1,88 @@
 package cc.yiueil.util;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * GlobalProperties 全局配置属性类
  * @author 弋孓 YIueil@163.com
  * @date 2023/5/31 23:22
  * @version 1.0
+ * 修订记录:
+ * v1.0: 实现从classpath中加载所有的配置文件
  */
+@Slf4j
 public class GlobalProperties {
+    /**
+     * 数据已加载标识
+     */
+    public static Boolean load = false;
 
+    /**
+     * 全局配置properties
+     */
+    public static Properties globalProperties = new Properties();
+
+    /**
+     * 默认路径加载配置文件: classpath:properties/*.properties
+     */
+    public static void load() {
+        load(null);
+    }
+
+    /**
+     * 指定配置文件路径加载配置文件
+     * @param locationPattern 指定配置文件路径
+     */
+    public static void load(String locationPattern) {
+        try {
+            Resource[] resources = SpringContextUtils.getResources(ObjectUtils.defaultIfNull(locationPattern, "classpath:properties/*.properties"));
+            if (ArrayUtils.isNotEmpty(resources)) {
+                for (Resource resource : resources) {
+                    globalProperties.load(resource.getInputStream());
+                }
+                log.debug(String.format("globalProperties: %s", globalProperties));
+                load = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 重新加载全局配置
+     */
+    public static void reload() {
+        reload(null);
+    }
+
+    /**
+     * 使用指定配置文件路径重新加载全局配置
+     * @param locationPattern 指定配置文件路径
+     */
+    public static void reload(String locationPattern) {
+        clear();
+        load(locationPattern);
+    }
+
+    private static void clear() {
+        globalProperties.clear();
+        load = false;
+    }
+
+    /**
+     * 全局获取配置项
+     * @param key 键值
+     * @param defaultValue 默认值
+     * @return value
+     */
+    public static String getProperties(String key, String defaultValue) {
+        if (!load) {
+            load(null);
+        }
+        return ObjectUtils.defaultIfNull(globalProperties.getProperty(key), defaultValue);
+    }
 }
--- yl-bdf-web/src/main/java/cc/yiueil/util/SpringContextUtils.java
+++ yl-bdf-web/src/main/java/cc/yiueil/util/SpringContextUtils.java
@@ -1,10 +1,14 @@
 package cc.yiueil.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * SpringContextUtils spring容器工具类
@@ -18,7 +22,7 @@ public class SpringContextUtils implements ApplicationContextAware {
     private static ApplicationContext applicationContext;
 
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    public void setApplicationContext(@NotNull ApplicationContext applicationContext) throws BeansException {
         SpringContextUtils.applicationContext = applicationContext;
     }
 
@@ -34,5 +38,15 @@ public class SpringContextUtils implements ApplicationContextAware {
 
     public static <T> T getBean(Class<T> cls) {
         return applicationContext.getBean(cls);
+    }
+
+    /**
+     * 获取到类路径资源
+     * @param locationPattern 类路径资源路径 eg: classpath:*.xml
+     * @return 类路径下匹配到的资源
+     * @throws IOException IO相关异常
+     */
+    public static Resource[] getResources(String locationPattern) throws IOException {
+        return applicationContext.getResources(locationPattern);
     }
 }
